370ea1cd6ddd48cd7723cef00c4198758c06820b
Juninho <jonildopereira5@gmail.com>
2025-04-09 17:29:57 -0300

    added CPF of the user entity

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -5,7 +5,8 @@ const schema = new Schema({
     name: {type: String, required: true},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
-    role: {type: String, required: true, default: 'user'},
+    role: {type: String, required: true, default: 'client'},
+    cpf: {type: String, required: true},
     createdAt: {type: Date, default: Date.now}, 
     updatedAt: {type: Date, default: Date.now},
 });
